19af2e25e52d2a03231fd140beb655643bef2449
kathiravan <mkathiravanmca@gmail.com>
2019-05-14 00:12:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kabali/bottomnavigation/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.kabali.bottomnavigation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import com.kotlincodes.bottomnavigationview.FavoriteFragment
+import com.kotlincodes.bottomnavigationview.HomeFragment
+import com.kotlincodes.bottomnavigationview.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        title = resources.getString(R.string.favorites)
+
+        loadFragment(FavoriteFragment())
+
+        navigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.navigation_fav -> {
+                    title=resources.getString(R.string.favorites)
+                    loadFragment(FavoriteFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.navigation_home -> {
+                    title = resources.getString(R.string.home)
+                    loadFragment(HomeFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.navigation_settings -> {
+                    title = resources.getString(R.string.settings)
+                    loadFragment(SettingsFragment())
+                    return@setOnNavigationItemSelectedListener true
+
+                }
+            }
+            false
+        }
+
+
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit();
+
+    }
+}
